62e5330b54528225d89684742939a27ed829581c
George Apostolopoulos <gapost@ipta.demokritos.gr>
2024-06-29 11:56:27 +0300

    Make h5disp more MATLAB compat
    - added 'min' display mode
    - size display as e.g. 2x5

--- src/h5disp.m
+++ src/h5disp.m
@@ -4,17 +4,23 @@
 #
 # Display contents of a HDF5 file.
 #
-# h5disp(filename) displays the metadata that describes the structure of the specified HDF5 file.
+# h5disp(filename) displays the metadata that describes the structure of
+# the specified HDF5 file.
 #
-# h5disp(filename,location) displays the metadata for the specified location within the file.
+# h5disp(filename,location) displays the metadata for the specified location
+# within the file.
+#
+# h5disp(filename,loc,mode), where mode is 'min', displays only the group and
+# dataset names. The default for mode is 'simple', which displays the dataset
+# metadata and attribute values.
 #
 # @seealso{h5info}
 # @end deftypefn
 
 
-function h5disp(filename,location,sz,varargin)
+function h5disp(filename,location,mode)
 
-if nargin<1 || nargin>2,
+if nargin<1 || nargin>3,
     print_usage();
 endif
 
@@ -22,10 +28,24 @@ if nargin == 1
     location = "/";
 endif
 
+if nargin < 3,
+  mode = 'simple';
+end
+
+mode = tolower(mode);
+if !(strcmp(mode,'simple') || strcmp(mode,'min')),
+  error("Invalid 'mode' option. Valid values: 'simple' or 'min'");
+end
+
 info = h5info(filename,location);
 
 disp(["HDF5 " filename]);
-__disph5__(info,0);
+if strcmp(mode,'simple'),
+  __disph5__(info,0);
+else
+  __disph5_min__(info,0);
+end
+
 
 endfunction
 
@@ -39,6 +59,16 @@ function disp_indented(name,value,depth)
     disp([char(ones(size(S,1),2*(depth+1))*' ') S]);
   endif
 end
+
+function s = disp_size(sz)
+  if isempty(sz),
+    s = "";
+  elseif length(sz)==1,
+    s = num2str(sz(1));
+  else
+    s = [num2str(sz(1)) num2str(sz(2:end),'x%d')];
+  endif
+endfunction
 
 function __dispAttr__(info, depth)
 n=numfields(info);
@@ -65,10 +95,10 @@ if isfield(info,"Groups"), # info is a group
 elseif isfield(info,"Dataspace"), # info is a dataset
     disp([indent "Dataset '" info.Name "'"]);
     __dispAttr__(info.Attributes, depth+1);
+    disp([indent "  Extent: " num2str(info.Dataspace.ExtentType)]);
+    disp([indent "  Size: " disp_size(info.Dataspace.Size)]);
+    disp([indent "  MaxSize: " disp_size(double(info.Dataspace.MaxSize))]);
     __disph5__(info.Datatype,depth+1);
-    disp([indent "  Extent: [" num2str(info.Dataspace.ExtentType) "]"]);
-    disp([indent "  Size: [" num2str(info.Dataspace.Size) "]"]);
-    disp([indent "  MaxSize: [" num2str(double(info.Dataspace.MaxSize)) "]"]);
     if !isempty(info.ChunkSize)
         disp([indent "  ChunkSize: [" num2str(info.ChunkSize) "]"]);
     endif
@@ -80,6 +110,22 @@ elseif isfield(info,"Class"), # info is a datatype
     if isfield(info,"Sign"), disp([indent "  Sign: " info.Sign]); end
     if isfield(info,"charSet"), disp([indent "  charSet: " info.charSet]); end
     if isfield(info,"Pading"), disp([indent "  Pading: " info.Pading]); end
+end
+
+endfunction
+
+function __disph5_min__(info, depth)
+
+indent = blanks(2*depth);
+
+if isfield(info,"Groups"), # info is a group
+    disp([indent "Group '" info.Name "'"]);
+    datasets = info.Datasets;
+    for i=1:size(datasets,1) __disph5_min__(datasets(i),depth+1); endfor
+    groups = info.Groups;
+    for i=1:size(groups,1) __disph5_min__(groups(i),depth+1); endfor
+elseif isfield(info,"Dataspace"), # info is a dataset
+    disp([indent "Dataset '" info.Name "'"]);
 end
 
 endfunction
